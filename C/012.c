3d92b176777ad2db64a8a6c24d893aafc4afe386
bhuvan-golhar <bhuvangolhar2025@gmail.com>
2026-04-14 20:54:28 +0530

    Create a C program to perform the modulus of two integers

---
+++ C/012.c
@@ -0,0 +1,12 @@
+// C program: Run-time modulus
+
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("Enter the value of a = "); 
+	scanf("%d",&a);
+  printf("Enter the value of b = "); 
+	scanf("%d",&b);
+	printf("Modulus = %d mod %d = %d",a,b,a%b);
+}
