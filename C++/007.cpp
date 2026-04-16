694f3394477b67f0c3fb461e2063baf538226f15
bhuvan-golhar <bhuvangolhar2025@gmail.com>
2026-04-16 18:02:25 +0530

    Create a C++ program to find the mod of two integers

---
+++ C++/007.cpp
@@ -0,0 +1,11 @@
+// C++ program: Compile-time modulus
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	a=43;
+	b=21;
+	cout<<"a = "<<a<<endl<<"b = "<<b<<endl<<"mod = "<<a%b;
+}
