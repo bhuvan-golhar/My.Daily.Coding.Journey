336cb00e2d5d247cdf6d02eedc67597a32b11947
bhuvan-golhar <bhuvangolhar2025@gmail.com>
2026-04-17 19:11:31 +0530

    Create a C++ program to find the difference of two integers

---
+++ C++/009.cpp
@@ -0,0 +1,13 @@
+// C++ program: Run-time subtraction
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Enter the value of a = ";
+	cin>>a;
+	cout<<"Enter the value of b = ";
+	cin>>b;
+	cout<<"Difference = "<<a<<" - "<<b<<" = "<<a-b;
+}
