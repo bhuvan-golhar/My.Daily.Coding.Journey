b8c3c6bcd1ca5acb78f6e6c58d4e1882a09246da
bhuvan-golhar <bhuvangolhar2025@gmail.com>
2026-04-17 19:14:45 +0530

    Create a C++ program to find the product of two integers

---
+++ C++/010.cpp
@@ -0,0 +1,13 @@
+// C++ program: Run-time multiplication
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Enter the value of a = ";
+	cin>>a;
+	cout<<"Enter the value of b = ";
+	cin>>b;
+	cout<<"Product = "<<a<<" x "<<b<<" = "<<a*b;
+}
