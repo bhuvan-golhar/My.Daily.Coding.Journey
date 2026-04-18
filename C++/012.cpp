4cfa305103190671f4a0945c8a6e1928f57f30da
bhuvan-golhar <bhuvangolhar2025@gmail.com>
2026-04-18 22:50:37 +0530

    Create a C++ program to find the remainder of two integers

---
+++ C++/012.cpp
@@ -0,0 +1,13 @@
+// C++ program: Run-time modulus
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Enter the value of a = ";
+	cin>>a;
+	cout<<"Enter the value of b = ";
+	cin>>b;
+	cout<<"Remainder = "<<a<<" mod "<<b<<" = "<<a%b;
+}
