785758e245c85f8f03b49caf64d58f3577bd1fa4
bhuvan-golhar <bhuvangolhar2025@gmail.com>
2026-04-22 17:02:26 +0530

    Create a class in Java to print string using a variable

---
+++ Java/001.java
@@ -0,0 +1,12 @@
+// Java class: Print using variable
+
+package projectFolder;
+
+public class PrintUsingVariable
+{
+    public static void main(String[] args)
+    {
+        String greeting = "Good Morning";
+        System.out.println(greeting);
+    }
+}
